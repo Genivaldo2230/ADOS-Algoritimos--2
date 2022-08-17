a495131f4db276fed60c37bc626016de897141c8
gda_a <gda_anjos@hotmail.com.br>
2022-08-17 13:00:25 -0300

    VESION 1.0

---
+++ Ado_1_Alg_2/src/main/java/com/senac/alg2/ado_1_algoritimos2/Ado_1_Algoritimos2.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.senac.alg2.ado_1_algoritimos2;
+
+import java.util.Scanner;
+
+/**
+ *Realize e exiba a soma de dois números inteiros (Obs: pode utilizar dois números fixos)
+ * @author gda_a
+ */
+public class Ado_1_Algoritimos2 {
+
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         
+         System.out.println("Digite Primeiro Nº:");
+         int  num1 = sc.nextInt();
+         
+         System.out.println("Digite Segundo  Nº:");
+         int num2 = sc.nextInt();
+         
+         
+         int soma = num1 + num2;
+         
+         System.out.println("Os Valores Somados : "+ soma);
+        
+        
+    }
+}
